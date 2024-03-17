05b19424129055d9fa2f70fd8164d58ef77f9651
-ˏˋ Maple ˊˎ <git@kalitsune.net>
2024-03-17 15:04:42 +0100

    📄 Updated the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import SocialCard from '$lib/components/SocialCard.svelte';
+</script>
+
 <svelte:head>
 	<title>Welcome to fabricated!</title>
 
@@ -5,42 +9,42 @@
 </svelte:head>
 
 <!--Actual content-->
-<div class="page-content text-box">
-	<p class="text title blur">fabricated.world</p>
-	<p class="text subtitle">- coming soon -</p>
-	<p class="text small">Check our ​<a href="blog">Blog</a></p>
-</div>
+<hgroup>
+	<h1>fabricated.world</h1>
+	<h2>Check our ​<a href="blog">Blog</a></h2>
+	<SocialCard
+		text="Join our server!"
+		url="https://discord.gg/t7U4xD7QyZ"
+		logo="https://assets-global.website-files.com/6257adef93867e50d84d30e2/653714c1f22aef3b6921d63d_636e0a6ca814282eca7172c6_icon_clyde_white_RGB.svg"
+		alt="discord"
+		back_color="#5865F2"
+		style="width: fit-content; margin-top: var(--size-2);"
+	/>
+</hgroup>
 
 <style>
-	.page-content {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-	}
-	.text-box {
+	hgroup {
+		width: 100%;
+		height: 100%;
+
+		gap: var(--size-2);
+
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+
+		padding: var(--size-7) var(--size-8);
 	}
-	.text {
-		color: var(--text-1);
-		font-family: 'JetBrains Mono', monospace;
-		display: inline-flex;
+	h1,
+	h2 {
+		font-family: var(--font-mono);
 	}
-	.title {
-		font-size: 100px;
-	}
-	.subtitle {
-		margin-top: 15px;
-		font-size: 50px;
-	}
-	.small {
-		margin-top: 5px;
-		font-size: 25px;
-	}
-	.blur {
+	h1 {
 		text-shadow: #0aa 0 0 50px;
+	}
+	h2 {
+		display: inline-flex;
+		flex-direction: row;
 	}
 </style>
